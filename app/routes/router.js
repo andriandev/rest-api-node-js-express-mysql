14ab596f986440aca6886d2e9af4ca40aec2733d
Andrian Dev <andrianpoint@gmail.com>
2022-03-25 19:16:22 +0700

    Memperbaiki sistem autoload controller di router.js

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const Controller = require('../controllers/Autoload.js');
+const Controller = require('../controllers/Autoload.js'); // Load semua controller
 
+// Membuat variabel single controller
+const Product = Controller.Product;
+
+// System Routing
 const router = express.Router();
 
-router.get('/', Controller.Product.getAllProducts);
-router.get('/page/:page', Controller.Product.getAllProducts);
-router.get('/:id', Controller.Product.getProductById);
-router.post('/', Controller.Product.createdProducts);
-router.put('/:id', Controller.Product.updateProduct);
-router.delete('/:id', Controller.Product.deleteProduct);
+router.get('/', Product.getAllProducts);
+router.get('/page/:page', Product.getAllProducts);
+router.get('/:id', Product.getProductById);
+router.post('/', Product.createdProducts);
+router.put('/:id', Product.updateProduct);
+router.delete('/:id', Product.deleteProduct);
 
 module.exports = router;
